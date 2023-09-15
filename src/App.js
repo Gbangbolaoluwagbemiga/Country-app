d63b136aec9f41f68f95ebac1a39ba237cce5955
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-09-15 21:26:13 +0100

    Project kickoff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import logo from './logo.svg';
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      <header className="nav--bar my-5">
+        <h2>Where in the world?</h2>
+        <p>Dark mode</p>
+      </header>
+      <main>
+        <div className="search--category">
+          <input
+            type="text"
+            placeholder="Search for a country"
+            spellCheck
+            className="Input--container"
+          />
+          <select className="Input--container">
+            <option value="Africa">Africa</option>
+            <option value="America">America</option>
+            <option value="Asia">Asia</option>
+            <option value="Europe">Europe</option>
+            <option value="Oceania">Ocenia</option>
+          </select>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
