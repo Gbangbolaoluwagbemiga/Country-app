e628c4b3802e6f0082da372545ed1fa09dfdb6a3
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-09-22 16:31:48 +0100

    Tbc

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 import {data} from './data.js';
-import {Numeral} from 'numeral';
+import {numeral} from 'numeral';
 function App() {
   const [countryOpen, setCountryOpen] = useState(false);
   const [countryId, setCountryId] = useState(null);
@@ -103,7 +103,7 @@ function CountryItem({country, onSetCountryId, countryId}) {
       />
       <p className="fw-bold fs-3 text-detail mt-5">{country.name}</p>
       <p className="line--adjustment">
-        Population: {Numeral(country.population).format('0,0')}
+        Population: {numeral(country.population).format('0,0')}
       </p>
       <p className="line--adjustment">Region: {country.region}</p>
       <p className="line--adjustment">Capital: {country.capital}</p>
