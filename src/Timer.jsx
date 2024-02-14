9c53cc00354e1c32c14714a26582ca5f21ba51c5
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-02-14 15:44:44 +0100

    Today init

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,36 +1,40 @@
 import {useEffect, useState} from 'react';
 
 function Timer() {
-  const [countDown, setCountDown] = useState(500);
+  const storedCountdown = localStorage.getItem('countdown');
+  const initialCountdown = storedCountdown
+    ? parseInt(storedCountdown, 10)
+    : 5000;
+
+  const [countDown, setCountDown] = useState(initialCountdown);
+
   const days = Math.floor(countDown / 86400);
   const hours = Math.floor((countDown % 86400) / 3600);
   const mins = Math.floor((countDown % 3600) / 60);
   const sec = countDown % 60;
 
-  useEffect(
-    function () {
-      const id = setInterval(() => {
-        setCountDown(prev => prev - 1);
-      }, 1000);
+  useEffect(() => {
+    localStorage.setItem('countdown', countDown.toString());
 
-      return () => clearInterval(id);
-    },
-    [countDown, setCountDown]
-  );
+    const id = setInterval(() => {
+      setCountDown(prev => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(id);
+  }, [countDown, setCountDown]);
 
   return (
-    <div className="time d-flex">
-      <span className=" me-3 color-start timer days">
+    <div className="time d-flex ">
+      <span className="me-3 color-start timer days">
         {days < 10 && '00'} <p className="time">Days</p>
       </span>{' '}
-      <span className=" me-3 color-end timer hours">
-        {' '}
+      <span className="me-3 color-end timer hours">
         {hours < 10 ? `0${hours} ` : hours} <p className="time">Hours </p>
       </span>{' '}
-      <span className=" me-3 color-end timer mins">
+      <span className="me-3 color-end timer mins">
         {mins < 10 ? `0${mins} ` : mins} <p className="time">Minutes </p>
       </span>{' '}
-      <span className=" me-3 color-start timer sec">
+      <span className="me-3 color-start timer sec">
         {sec < 10 && '0'}
         {sec} <p className="time">Seconds</p>
       </span>{' '}
